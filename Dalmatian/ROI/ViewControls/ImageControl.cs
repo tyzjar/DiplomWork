3d8704622100df4e5b302523408d43b4f008d3a2
tyzjar <tyzjar1@gmail.com>
2022-04-18 11:10:46 +0400

    b2

--- Dalmatian/ROI/ViewControls/ImageControl.cs
+++ Dalmatian/ROI/ViewControls/ImageControl.cs
@@ -8,5 +8,22 @@ namespace Dalmatian.ROI
 {
    class ImageControl
    {
+
+
+      //public void MouseRightPress(object sender, RoutedEventArgs e)
+      //{
+      //   var draggableControl = sender as UserControl;
+      //   mouseState.p_rightPressed = (e as MouseEventArgs).GetPosition(canvasFrame);
+      //   MessageBox.Show(draggableControl.Name);
+      //   draggableControl.CaptureMouse();
+      //   MessageBox.Show("2");
+      //   mouseState.rightPressed = true;
+      //}
+      //public void MouseRightUp(object sender, RoutedEventArgs e)
+      //{
+      //   mouseState.rightPressed = false;
+      //   var draggable = sender as UserControl;
+      //   draggable.ReleaseMouseCapture();
+      //}
    }
 }
